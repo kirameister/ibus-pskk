0a572e949c85aa6fd495a8b88b0cb45c8a975e4d
Akira K. <kira@kirameister.net>
2025-03-24 06:25:37 +0100

    update unit-test

--- engine/test_engine.py
+++ engine/test_engine.py
@@ -1,7 +1,13 @@
 import unittest
+from unittest.mock import patch, MagicMock
 import engine
 from unittest import TestCase
 from gi.repository import IBus
+
+import gi
+gi.require_version("IBus", "1.0")
+from gi.repository import IBus
+
 
 import logging
 logger = logging.getLogger()
@@ -10,7 +16,7 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(m
 RELEASE_ACTION = IBus.ModifierType.RELEASE_MASK
 PRESS_ACTION   = 0
 
-""" Defined set of constants
+# Defined set of constants
 STATUS_SPACE        = 0x01
 STATUS_SHIFT_L      = 0x02 # this value is currently not meant to be used directly
 STATUS_SHIFT_R      = 0x04 # this value is currently not meant to be used directly
@@ -30,27 +36,74 @@ MODE_JUST_FINISHED_KANCHOKU                    = 0x010
 MODE_IN_CONVERSION                             = 0x020
 MODE_IN_FORCED_CONVERSION                      = 0x040
 SWITCH_FIRST_SHIFT_PRESSED_IN_PREEDIT          = 0x080
-"""
 
-class Test_simplest_strokes(unittest.TestCase):
-    """
-    """
+
+class TestSimplestStrokes(unittest.TestCase):
     def setUp(self):
+        """Let the _commit_string() and _update_preedit() functions do nothing"""
+        # Initialize the object under test after mocking
         self.eq = engine.EnginePSKK()
+        # Mock _commit_string and _update_preedit to avoid real execution
+        self.eq._commit_string = MagicMock(return_value=None)
+        self.eq._update_preedit = MagicMock(return_value=None)
+        self.eq.commit_text = MagicMock(return_value=None)
+
+    def tearDown(self):
+        """Nothing to be done by this function"""
+        #self.patcher.stop()
+        pass
 
     def test_process_key_event_1(self):
-        exp_return_val = True
+        """ P(a) => 'の' / '' """
+        exp_preedit = 'の'
+        self.eq._modkey_status = 0
+        self.eq._typing_mode = 0
+        self.assertEqual(self.eq.process_key_event(IBus.a, PRESS_ACTION), True)
+        self.assertEqual(exp_preedit, self.eq._preedit_string)
+        self.assertEqual(self.eq._modkey_status, 0)
+        self.assertEqual(self.eq._typing_mode, 0)
+        self.eq._update_preedit.assert_called() # Fails if never called
+        self.eq._commit_string.assert_called()
+
+    def test_process_key_event_2(self):
+        """ P(a)R(a) => 'の' / '' """
+        exp_preedit = 'の'
+        self.eq._modkey_status = 0
+        self.eq._typing_mode = 0
+        self.assertEqual(self.eq.process_key_event(IBus.a, PRESS_ACTION), True)
+        self.assertEqual(self.eq.process_key_event(IBus.a, RELEASE_ACTION), True)
+        self.assertEqual(exp_preedit, self.eq._preedit_string)
+        self.assertEqual(self.eq._modkey_status, 0)
+        self.assertEqual(self.eq._typing_mode, 0)
+        self.eq._update_preedit.assert_called()
+        self.eq._commit_string.assert_called()
+
+    def test_process_key_event_3(self):
+        """ P(space) => '' / '' (漢直モード) """
         exp_preedit = ''
         self.eq._modkey_status = 0
         self.eq._typing_mode = 0
-        actual_return_val = self.eq.process_key_event(IBus.a, PRESS_ACTION)
-        actual_return_val = self.eq.process_key_event(IBus.a, RELEASE_ACTION)
-        actual_return_val = self.eq.process_key_event(IBus.k, PRESS_ACTION)
-        actual_return_val = self.eq.process_key_event(IBus.k, RELEASE_ACTION)
-        print(f'actual_return_val: {actual_return_val}')
-        self.assertEqual(exp_return_val, actual_return_val)
+        self.assertEqual(self.eq.process_key_event(IBus.space, PRESS_ACTION), True)
         self.assertEqual(exp_preedit, self.eq._preedit_string)
-        self.tn
+        self.assertEqual(self.eq._modkey_status, STATUS_SPACE)
+        self.assertEqual(self.eq._typing_mode, MODE_IN_PREEDIT | SWITCH_FIRST_SHIFT_PRESSED_IN_PREEDIT)
+        self.eq.commit_text.assert_not_called()
+        self.eq._update_preedit.assert_not_called()
+        self.eq._commit_string.assert_not_called()
+
+    def test_process_key_event_4(self):
+        """ P(space)R(space) => '' / 'space' """
+        exp_preedit = ''
+        self.eq._modkey_status = 0
+        self.eq._typing_mode = 0
+        self.assertEqual(self.eq.process_key_event(IBus.space, PRESS_ACTION), True)
+        self.assertEqual(self.eq.process_key_event(IBus.space, RELEASE_ACTION), True)
+        self.assertEqual(exp_preedit, self.eq._preedit_string)
+        self.assertEqual(self.eq._modkey_status, 0)
+        self.assertEqual(self.eq._typing_mode, SWITCH_FIRST_SHIFT_PRESSED_IN_PREEDIT)
+        self.eq.commit_text.assert_called()
+        self.eq._update_preedit.assert_not_called()  # Fails if never called
+        self.eq._commit_string.assert_not_called()
 
 
 if(__name__ == '__main__'):
