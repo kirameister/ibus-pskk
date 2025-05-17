59765cc32dad3568eef48b133b77c53cea5244f7
Akira K. <kira@kirameister.net>
2025-05-17 15:12:43 +0200

    Comment updates

--- engine/engine.py
+++ engine/engine.py
@@ -676,20 +676,13 @@ class EnginePSKK(IBus.Engine):
         This function is called when there is a key-storke event from IBus.
         This is almost a wrapper function to process_key_event()
         """
-        #return self._event.process_key_event(keyval, keycode, state)
         is_press_action = ((state & IBus.ModifierType.RELEASE_MASK) == 0)
-        '''
-        if(is_press_action):
-            logger.debug(f'do_process_key_event -- press ("{IBus.keyval_name(keyval)}", {keyval:#04x}, {keycode:#04x}, {state:#010x})')
-        else:
-            logger.debug(f'do_process_key_event -- release ("{IBus.keyval_name(keyval)}", {keyval:#04x}, {keycode:#04x}, {state:#010x})')
-        '''
         # 変換 / 無変換
         if(keyval == IBus.Muhenkan):
             if(is_press_action): # this extra if-clause is necessary not to cascade release signal to further function.
                 logger.debug(f'do_process_key_event -- IME set disabled via Muhenkan')
                 self.set_mode('A', True)
-            return(True)
+            return(True) # At this point, no further process would take place. 
         if(keyval == IBus.Henkan or keyval == IBus.Henkan_Mode):
             if(is_press_action):
                 logger.debug(f'do_process_key_event -- IME set enabled via Henkan')
@@ -701,6 +694,7 @@ class EnginePSKK(IBus.Engine):
         if(not self.is_enabled()):
             # this block is for direct-mode (no Japanese char)
             return(False)
+        # Following if-clause with Super key should be in this function as Super+space would be used for IME-toggles (by IBus)
         if(keyval == IBus.Super_L or keyval == IBus.Super_R):
             if(is_press_action):
                 self._modkey_status |= STATUS_SUPERS
