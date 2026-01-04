993aa7bfba2bc142fdaee1059683080442b73fec
Akira K. <kira@kirameister.net>
2026-01-04 01:17:24 +0100

    Big change - remove resources originally contained in IBus-Hiragana

--- src/util.py
+++ src/util.py
@@ -1,22 +1,3 @@
-# ibus-pskk - PSKK for IBus
-#
-# Copyright (c) 2020 Esrille Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the License);
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at:
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an AS IS BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# This file is a template file processed by the configure script.
-# The file name is listed in AC_CONFIG_FILES of configure.ac.
-
 import codecs
 import json
 import os
